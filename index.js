acf4e7f82a23def4058fdda4012f8cf5663c2478
AyuburRahman <aiburrahman17@gmail.com>
2022-03-19 16:08:34 +0600

    added-navbar

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wsmmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+
 
 async function run() {
     try {
